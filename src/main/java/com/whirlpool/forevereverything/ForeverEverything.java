7d183cc86598ca10fda735c383892a9fa59c954f
TerabyteTB <raymondminecraft101@gmail.com>
2021-03-29 16:42:44 -0700

    add custom tab

--- src/main/java/com/whirlpool/forevereverything/ForeverEverything.java
+++ src/main/java/com/whirlpool/forevereverything/ForeverEverything.java
@@ -1,6 +1,8 @@
 package com.whirlpool.forevereverything;
 
 import com.whirlpool.forevereverything.util.RegistryHandler;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -27,5 +29,12 @@ public class ForeverEverything
     private void setup(final FMLCommonSetupEvent event) {}
 
     private void doClientStuff(final FMLClientSetupEvent event) {}
+
+    public static final ItemGroup TAB = new ItemGroup("foreverEverythingTab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(RegistryHandler.ADAMANT.get());
+        }
+    };
 
 }
--- src/main/java/com/whirlpool/forevereverything/items/ItemBase.java
+++ src/main/java/com/whirlpool/forevereverything/items/ItemBase.java
@@ -1,11 +1,12 @@
 package com.whirlpool.forevereverything.items;
 
+import com.whirlpool.forevereverything.ForeverEverything;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class ItemBase extends Item {
 
     public ItemBase() {
-        super(new Properties().tab(ItemGroup.TAB_MATERIALS));
+        super(new Properties().tab(ForeverEverything.TAB));
     }
 }
