d64ed15e1becd4403f9c2708e1d073abd428263c
TerabyteTB <raymondminecraft101@gmail.com>
2021-03-28 20:10:26 -0700

    commit

---
+++ src/main/java/com/whirlpool/forevereverything/ForeverEverything.java
@@ -0,0 +1,31 @@
+package com.whirlpool.forevereverything;
+
+import com.whirlpool.forevereverything.util.RegistryHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("fe4mc")
+public class ForeverEverything
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "fe4mc";
+
+    public ForeverEverything() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        RegistryHandler.init();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {}
+
+    private void doClientStuff(final FMLClientSetupEvent event) {}
+
+}
---
+++ src/main/java/com/whirlpool/forevereverything/util/RegistryHandler.java
@@ -0,0 +1,22 @@
+package com.whirlpool.forevereverything.util;
+
+import com.whirlpool.forevereverything.ForeverEverything;
+import com.whirlpool.forevereverything.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ForeverEverything.MOD_ID);
+
+    public static void init(){
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    //Items
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+}
